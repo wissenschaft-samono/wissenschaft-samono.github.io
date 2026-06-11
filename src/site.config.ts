6efe8caa12a4444970f93960d2db9303088d311d
Yuto Yokoyama <yuto@MacBuch-Leicht.local>
2026-06-11 23:37:25 +0900

    Refine site profile description

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,7 +2,7 @@ export const SITE = {
   title: '風呂は命の洗濯',
   subtitle: '杜の都に潜む大学院生の哲学と文学についての覚え書き',
   author: 'さもの',
-  description: '日本学を専攻する修士課程大学院生です。現象学を中心とした哲学のなかで文学や倫理について考えたいと思っています。',
+  description: '日本学を専攻する修士課程の大学院生です。現象学を中心とした哲学のなかで文学や倫理について考えたいと思っています。',
   locale: 'ja-JP',
   timezone: 'Asia/Tokyo',
   url: 'https://wissenschaft-samono.github.io',
